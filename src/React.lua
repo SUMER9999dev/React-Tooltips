df0fc949e9537dbe48701f871b389cb6f7fe0408
SUMER <9999smexov@gmail.com>
2024-07-05 19:53:09 +0300

    feat: added support for ts react.

--- src/React.lua
+++ src/React.lua
@@ -1,12 +1,21 @@
 local Packages = script.Parent.Parent
+
+Packages = if Packages:FindFirstChild('React') then Packages else Packages:FindFirstChild('ReactLua')
+assert(Packages, 'Failed to find packages.')
+
 local React = Packages:FindFirstChild('React')
-
 assert(React, 'Failed to find react dependency.')
-
 
 local ReactLibrary = require(React)
 
-export type ComponentType<T> = ReactLibrary.ComponentType<T>
-export type Binding<T> = ReactLibrary.Binding<T>
+export type ComponentType<T> = typeof((function()
+	local React = require(script.Parent.Parent.React)
+	return ({} :: any) :: React.ComponentType<T>
+end)())
+
+export type Binding<T> = typeof((function()
+	local React = require(script.Parent.Parent.React)
+	return {} :: React.Binding<T>
+end)())
 
 return ReactLibrary
